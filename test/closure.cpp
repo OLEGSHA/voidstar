39dff752a2fadec0329d4b296d289bcfa1db8d7a
OLEGSHA <kvadropups@gmail.com>
2025-06-12 23:28:12 +0200

    Added first batch of tests

---
+++ test/closure.cpp
@@ -0,0 +1,115 @@
+#include <gtest/gtest.h>
+
+#include <voidstar.h>
+
+#include <future>
+#include <list>
+#include <memory>
+#include <thread>
+
+namespace voidstar::test {
+
+TEST(closure, lifetime) {
+  struct payload {
+    void operator()() {}
+  };
+  closure<void(), payload> object;
+  (void)object;
+}
+
+TEST(closure, payload_lifetime) {
+  struct snowflake {};
+
+  int value = 0;
+
+  struct payload {
+    int &value;
+    payload(int &value, snowflake) : value{value} { value++; }
+    ~payload() { value++; }
+    void operator()() {}
+  };
+
+  EXPECT_EQ(value, 0);
+  {
+    closure<void(), payload> object{value, snowflake{}};
+    EXPECT_EQ(value, 1);
+  }
+  EXPECT_EQ(value, 2);
+}
+
+TEST(closure, simple_call) {
+  int calls = 0;
+
+  struct payload {
+    int &calls;
+    payload(int &calls) : calls{calls} {}
+    void operator()() { calls++; }
+  };
+
+  EXPECT_EQ(calls, 0);
+  closure<void(), payload> cls{calls};
+  EXPECT_EQ(calls, 0);
+  cls.get()();
+  EXPECT_EQ(calls, 1);
+}
+
+TEST(closure, make_closure) {
+  int calls = 0;
+
+  EXPECT_EQ(calls, 0);
+  auto cls = make_closure<void()>([&] { calls++; });
+  EXPECT_EQ(calls, 0);
+  cls.get()();
+  EXPECT_EQ(calls, 1);
+}
+
+TEST(closure, many_closures) {
+  constexpr std::size_t N = 1000;
+
+  std::array<int, N> targets{};
+
+  auto make_payload = [&](int i) {
+    return [i, &targets] { targets.at(i) = i; };
+  };
+  using cls_t = closure<void(), decltype(make_payload(0))>;
+
+  std::list<cls_t> clses;
+  for (std::size_t i = 0; i < N; i++) {
+    clses.emplace_back(make_payload(i));
+  }
+
+  for (std::size_t i = 0; i < N; i++) {
+    EXPECT_EQ(targets[i], 0);
+  }
+
+  for (auto const &cls : clses) {
+    cls.get()();
+  }
+
+  for (std::size_t i = 0; i < N; i++) {
+    EXPECT_EQ(targets[i], i);
+  }
+}
+
+TEST(closure, no_thread_locality) {
+  // Ensure that thread of origin and the thread of use are not important
+
+  // std::jthread instead of std::async or similar to guarantee no thread reuse
+  auto run_in_new_thread = [](auto fn) { (void)std::jthread{fn}; };
+
+  int calls = 0;
+
+  auto payload = [&] { calls++; };
+  using cls_t = closure<void(), decltype(payload)>;
+  std::unique_ptr<cls_t> cls;
+
+  run_in_new_thread([&] { cls = std::make_unique<cls_t>(payload); });
+
+  EXPECT_EQ(calls, 0);
+  run_in_new_thread([&] { cls->get()(); });
+  EXPECT_EQ(calls, 1);
+  run_in_new_thread([&] { cls->get()(); });
+  EXPECT_EQ(calls, 2);
+}
+
+} // namespace voidstar::test
---
+++ test/closure.static.cpp
@@ -0,0 +1,180 @@
+#include <voidstar.h>
+
+#include <type_traits>
+
+namespace voidstar::test {
+
+////////////////////////////////////////////////////////////////////////////////
+// Type properties
+//
+
+// clang-format off
+template <typename T, typename P>
+concept correct_closure = //
+  std::is_destructible_v<P>
+  and std::is_default_constructible_v<T> == std::is_default_constructible_v<P>
+  and not std::is_copy_constructible_v<T>
+  and not std::is_copy_assignable_v<T>
+  and not std::is_move_constructible_v<T>
+  and not std::is_move_assignable_v<T>
+
+  and requires {
+    typename T::call_signature;
+    typename T::fn_ptr_type;
+    typename T::payload;
+  }
+
+  and std::same_as<typename T::payload, P>
+  and matches<P, typename T::call_signature>
+
+  and std::is_pointer_v<typename T::fn_ptr_type>
+  and std::is_function_v<std::remove_pointer_t<typename T::fn_ptr_type>>
+
+  and requires(T closure, typename T::fn_ptr_type& fn_ptr_out) {
+    {closure.get()} -> std::convertible_to<typename T::fn_ptr_type>;
+    {fn_ptr_out = closure};
+  };
+// clang-format on
+
+////////////////////////////////////////////////////////////////////////////////
+// Substitutions
+//
+
+template <typename F, typename P>
+concept closure_valid = correct_closure<closure<F, P>, P>;
+
+template <typename F, typename P>
+concept closure_invalid = not requires {
+  typename closure<F, P>;
+};
+
+// Trivial
+static_assert(closure_valid<void(), decltype([] {})>);
+
+// Trivial with argument
+static_assert(closure_valid<void(int), decltype([](int) {})>);
+
+// Multiple arguments
+static_assert(closure_valid<void(int, float *, double),
+                            decltype([](int, float *, double) {})>);
+static_assert(closure_valid<void(int, int, int), //
+                            decltype([](int, int, int) {})>);
+static_assert(closure_invalid<void(int, float *), //
+                              decltype([](int, float *, double) {})>);
+static_assert(closure_invalid<void(int, float *, double), //
+                              decltype([](int, float *) {})>);
+
+// Pointer arguments
+static_assert(closure_valid<void(int *), decltype([](int *) {})>);
+static_assert(closure_valid<void(void *), decltype([](void *) {})>);
+static_assert(closure_valid<void(int ***), decltype([](int ***) {})>);
+
+// Constness in pointer arguments
+static_assert(closure_invalid<void(int const *), decltype([](int *) {})>);
+static_assert(closure_valid<void(int *), decltype([](int const *) {})>);
+static_assert(closure_valid<void(int const *), decltype([](int const *) {})>);
+
+// Volatility in pointer arguments
+static_assert(closure_invalid<void(int volatile *), decltype([](int *) {})>);
+static_assert(closure_valid<void(int *), decltype([](int volatile *) {})>);
+static_assert(closure_valid<void(int volatile *), //
+                            decltype([](int volatile *) {})>);
+static_assert(closure_valid<void(int const volatile *),
+                            decltype([](int const volatile *) {})>);
+
+// Compatible arguments
+static_assert(
+    closure_valid<void(std::uint8_t), decltype([](std::uint64_t) {})>);
+static_assert(
+    closure_valid<void(std::uint64_t), decltype([](std::uint8_t) {})>);
+static_assert( //
+    closure_valid<void(int ***), decltype([](void const *) {})>);
+static_assert( //
+    closure_invalid<void(int), decltype([](int *) {})>);
+
+// Array argument
+static_assert(closure_valid<void(int[10]), decltype([](int[10]) {})>);
+static_assert(closure_invalid<void(int[10]), decltype([](float[10]) {})>);
+static_assert(closure_invalid<void(int[10]), decltype([](int) {})>);
+
+// Array decay
+static_assert(closure_valid<void(int *), decltype([](int[10]) {})>);
+static_assert(closure_valid<void(int[10]), decltype([](int *) {})>);
+static_assert(closure_valid<void(int[10]), decltype([](int[5]) {})>);
+
+// Unbounded array argument
+static_assert(closure_valid<void(int[]), decltype([](int[]) {})>);
+static_assert(closure_valid<void(int *), decltype([](int[]) {})>);
+static_assert(closure_valid<void(int[]), decltype([](int *) {})>);
+static_assert(closure_valid<void(int[10]), decltype([](int[]) {})>);
+static_assert(closure_valid<void(int[]), decltype([](int[10]) {})>);
+
+// Deductions, etc.
+static_assert(closure_valid<void(), decltype([](int = 10) {})>);
+static_assert(closure_valid<void(), decltype([](...) {})>);
+static_assert(closure_valid<void(int), decltype([](auto) {})>);
+static_assert(closure_invalid<void(), decltype([](auto) {})>);
+static_assert(
+    closure_invalid<void(float), decltype([](std::integral auto) {})>);
+
+// Return types
+static_assert(closure_valid<int(), decltype([] { return int{}; })>);
+static_assert(closure_invalid<int(), decltype([] {})>);
+static_assert(closure_invalid<void(), decltype([] { return int{}; })>);
+
+static_assert(closure_valid<int *(), decltype([] { return (int *){}; })>);
+static_assert(closure_valid<int const *(), decltype([] { return (int *){}; })>);
+static_assert(
+    closure_valid<std::uint8_t(), decltype([] { return std::uint8_t{}; })>);
+static_assert(
+    closure_valid<std::uint64_t(), decltype([] { return std::uint64_t{}; })>);
+
+// Compatible return type
+static_assert(
+    closure_valid<std::uint8_t(), decltype([] { return std::uint64_t{}; })>);
+static_assert(
+    closure_valid<std::uint64_t(), decltype([] { return std::uint8_t{}; })>);
+static_assert(closure_invalid<int *(), decltype([] { return int{}; })>);
+
+// Other callables
+static_assert(closure_valid<void(), void (*)()>);
+static_assert(closure_valid<void(), std::function<void()>>);
+
+struct minimal_callable_vi {
+  virtual void operator()(int);
+
+  minimal_callable_vi(minimal_callable_vi const &) = delete;
+  auto operator=(minimal_callable_vi const &) -> minimal_callable_vi & = delete;
+  minimal_callable_vi(minimal_callable_vi &&) = delete;
+  auto operator=(minimal_callable_vi &&) -> minimal_callable_vi & = delete;
+};
+
+static_assert(closure_valid<void(int), minimal_callable_vi>);
+
+// Bad callables
+static_assert(closure_invalid<void(), int>);
+static_assert(closure_invalid<void(), void>);
+static_assert(closure_invalid<void(), int (*)()>);
+static_assert(closure_invalid<void(), void (*)(int)>);
+
+// Bad function signatures
+static_assert(closure_invalid<void, void (*)()>);
+static_assert(closure_invalid<int, void (*)()>);
+static_assert(closure_invalid<int, int>);
+
+// Not yet supported
+static_assert(closure_invalid<void(...), decltype([](...) {})>);
+static_assert(closure_invalid<void(int, ...), decltype([](int, ...) {})>);
+
+// Other function signatures
+static_assert(closure_valid<void (*)(), decltype([] {})>);
+
+extern "C" {
+using c_fn_vv = void();
+using c_fn_ptr_vv = void (*)();
+}
+
+static_assert(closure_valid<c_fn_vv, decltype([] {})>);
+static_assert(closure_valid<c_fn_ptr_vv, decltype([] {})>);
+
+} // namespace voidstar::test
