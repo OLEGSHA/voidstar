13afceb22f9cb82f0b172906cb30a707b4011eca
OLEGSHA <kvadropups@gmail.com>
2025-07-02 22:22:10 +0200

    Added inline docs for call_signature.h

--- include/voidstar/detail/call_signature.h
+++ include/voidstar/detail/call_signature.h
@@ -13,33 +13,51 @@
 
 namespace voidstar::detail {
 
+// TODO: Differentiate `extern "C"` and `extern "C++"` function types on
+// supporting compilers (e.g. Oracle)
+
+/// @brief `extern "C++"` non-variadic function type.
 template <typename R, typename... T> using default_abi_fn = R(T...);
+
+/// @brief `extern "C++"` variadic function type.
 template <typename R, typename... T> using default_abi_var_fn = R(T..., ...);
 
-template <typename> struct call_signature;
+/// @brief Function traits for a function specifier @a F.
+template <typename F> struct call_signature;
 
+/// @brief Function traits for non-variadic function types.
 template <typename R, typename... T>
 struct call_signature<default_abi_fn<R, T...>> {
   using return_type = std::remove_cv_t<R>;
   using arg_types = std::tuple<std::decay_t<T>...>;
   static constexpr std::size_t arg_count = sizeof...(T);
 
+  /// @brief Pointer-to-function type described by these properties.
   using fn_ptr_type = default_abi_fn<R, std::decay_t<T>...> *;
 };
 
+/// @brief Function traits for variadic function types.
 template <typename R, typename... T>
 struct call_signature<default_abi_var_fn<R, T...>> {
   static_assert(dependent_false<R>::value,
                 "Variadic functions are not yet supported");
 };
 
+/// @brief Function traits for pointer-to-function types.
 template <typename T> struct call_signature<T *> : call_signature<T> {};
 
-template <typename P, typename> struct can_std_apply;
+/**
+ * @brief Determines whether `std::apply(p_val, t_val)` is well-formed for
+ * tuple-like @a T.
+ */
+template <typename P, typename T> struct can_std_apply;
 
 template <typename P, typename... A>
 struct can_std_apply<P, std::tuple<A...>> : std::is_invocable<P, A...> {};
 
+/**
+ * @brief Closure payload that can service trampolines with call signature @a C.
+ */
 // clang-format off
 template <typename P, typename C>
 concept matches =
@@ -48,10 +66,11 @@ concept matches =
     typename C::arg_types;
   }
 
+  // Avoid hard errors if std::apply fails
   and can_std_apply<P, typename C::arg_types>::value
 
   and requires(P payload, typename C::arg_types const &arg_tuple) {
-    {std::apply(payload, arg_tuple)}
+    {std::apply(payload, arg_tuple)} // TODO: static_cast ins and outs
       -> std::convertible_to<typename C::return_type>;
   };
 // clang-format on
