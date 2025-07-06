54e76ce675a294d3eb216c50f2dd52628f5cb9e1
OLEGSHA <kvadropups@gmail.com>
2025-07-06 20:02:36 +0200

    Formatted CMake stuff with cmake-format

--- cmake/Findlibffi.cmake
+++ cmake/Findlibffi.cmake
@@ -19,36 +19,32 @@ if(PKG_CONFIG_FOUND)
   pkg_check_modules(PC_libffi QUIET libffi)
 endif()
 
-find_path(libffi_INCLUDE_DIR
+find_path(
+  libffi_INCLUDE_DIR
   NAMES ffi.h
   HINTS ${PC_libffi_INCLUDE_DIRS}
-  PATH_SUFFIXES ffi
-)
-find_library(libffi_LIBRARY
+  PATH_SUFFIXES ffi)
+
+find_library(
+  libffi_LIBRARY
   NAMES ffi
-  HINTS ${PC_libffi_LIBRARY_DIRS}
-)
+  HINTS ${PC_libffi_LIBRARY_DIRS})
 
 set(libffi_VERSION ${PC_libffi_VERSION})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libffi
-  REQUIRED_VARS
-    libffi_LIBRARY
-    libffi_INCLUDE_DIR
-  VERSION_VAR libffi_VERSION
-)
+find_package_handle_standard_args(
+  libffi
+  REQUIRED_VARS libffi_LIBRARY libffi_INCLUDE_DIR
+  VERSION_VAR libffi_VERSION)
 
 if(libffi_FOUND AND NOT TARGET libffi::libffi)
   add_library(libffi::libffi UNKNOWN IMPORTED)
-  set_target_properties(libffi::libffi PROPERTIES
-    IMPORTED_LOCATION "${libffi_LIBRARY}"
-    INTERFACE_COMPILE_OPTIONS "${PC_libffi_CFLAGS_OTHER}"
-    INTERFACE_INCLUDE_DIRECTORIES "${libffi_INCLUDE_DIR}"
-  )
+  set_target_properties(
+    libffi::libffi
+    PROPERTIES IMPORTED_LOCATION "${libffi_LIBRARY}"
+               INTERFACE_COMPILE_OPTIONS "${PC_libffi_CFLAGS_OTHER}"
+               INTERFACE_INCLUDE_DIRECTORIES "${libffi_INCLUDE_DIR}")
 endif()
 
-mark_as_advanced(
-  libffi_INCLUDE_DIR
-  libffi_LIBRARY
-)
+mark_as_advanced(libffi_INCLUDE_DIR libffi_LIBRARY)
